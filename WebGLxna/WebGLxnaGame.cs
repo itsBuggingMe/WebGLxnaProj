096c6efa15f8dca3edfc0774bc93e66715f6950c
Paperclip <131926845+itsBuggingMe@users.noreply.github.com>
2023-12-04 12:43:46 +0000

    pong lol

--- WebGLxna/WebGLxnaGame.cs
+++ WebGLxna/WebGLxnaGame.cs
@@ -6,51 +6,52 @@ using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Input.Touch;
+using System.Security.Cryptography.X509Certificates;
 
 namespace WebGLxna
 {
     public class WebGLxnaGame : Game
     {
         private GraphicsDeviceManager graphics;
-
-        TriangleComponent _tri;
-        QuadTxComponent _triTx;
-        SpriteComponent _sprite;
-        SpriteFontComponent _spriteFont;
-        RTComponent _rt;
-        BatchTilesComponent _batchTilesComponent;
-
+        private SpriteBatch _sb;
         public WebGLxnaGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
-            _tri = new TriangleComponent(this);
-            _triTx = new QuadTxComponent(this);
-            _sprite = new SpriteComponent(this);
-            _spriteFont = new SpriteFontComponent(this);
-            _rt = new RTComponent(this);
-            _batchTilesComponent = new BatchTilesComponent(this);
-            //Components.Add(_tri);
-            Components.Add(_triTx);
-            //Components.Add(_sprite);
-            //Components.Add(_spriteFont);
-            //Components.Add(_rt);
-            //Components.Add(_batchTilesComponent);
-
             //tests
-            var wnd = this.Window;
+            var wnd = Window;
             var wndbounds = wnd.ClientBounds;
 
-            return;
+            location = wndbounds.Size.ToVector2() * 0.5f;
         }
 
+        private Texture2D square;
+        private Paddle leftPaddle;
+        private Paddle rightPaddle;
         protected override void Initialize()
         {
+            _sb = new SpriteBatch(graphics.GraphicsDevice);
+            square = generate();
             base.Initialize();
+
+            leftPaddle = new Paddle(new Rectangle(72, 64, 32, 196), Color.Purple);
+            rightPaddle = new Paddle(new Rectangle(1294, 64, 32, 196), Color.Yellow);
         }
 
 
+        private Texture2D generate(int size = 32)
+        {
+            Texture2D t =  new Texture2D(graphics.GraphicsDevice, size, size);
+            Color[] data = new Color[size * size];
+            for(int i = 0; i < data.Length; i++)
+            {
+                data[i] = Color.White;
+            }
+            t.SetData(data);
+            return t;
+        }
+        
         protected override void Update(GameTime gameTime)
         {
             MouseState ms = Mouse.GetState();
@@ -67,15 +68,87 @@ namespace WebGLxna
                 catch (PlatformNotSupportedException) { /* ignore */ }
             }
 
+            leftPaddle.Update(Keys.W, Keys.S);
+            rightPaddle.Update(Keys.I, Keys.K);
+            Rectangle ballBound = square.Bounds;
+            ballBound.Location += location.ToPoint();
+
+            deltaV *= leftPaddle.Collison(ballBound);
+            deltaV *= rightPaddle.Collison(ballBound);
+            location += deltaV;
+            if(location.X < 0 || (location.X + square.Width) >= graphics.PreferredBackBufferWidth)
+            {
+                deltaV *= Vector2.Zero;
+            }
+            if(location.Y < 0 || (location.Y + square.Height) >= graphics.PreferredBackBufferHeight)
+            {
+                deltaV *= new Vector2(1,-1);
+            }
+            deltaV.Normalize();
+            deltaV *= (MathF.Sin((float)gameTime.TotalGameTime.TotalSeconds * 0.3f) + 2)*4;
+            
             base.Update(gameTime);
         }
+
+        Vector2 deltaV = Vector2.One * 4;
+        Vector2 location = Vector2.Zero;
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.SetRenderTarget(null);
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            if(Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                GraphicsDevice.Clear(Color.Red);
+            }
+
+            _sb.Begin();
+            _sb.Draw(square, location, Color.White);
+            leftPaddle.Draw(_sb, square);
+            rightPaddle.Draw(_sb, square);
+            _sb.End();
+
             base.Draw(gameTime);
+        }
+    }
+
+        internal class Paddle
+    {
+        private Rectangle Bounds;
+        private Color color;
+        public Paddle(Rectangle bounds, Color Color)
+        {
+            Bounds = bounds;
+            color = Color;
+        }
+
+        public void Update(Keys up, Keys down)
+        {
+            KeyboardState keyState = Keyboard.GetState();
+            if(keyState.IsKeyDown(up))
+            {
+                Bounds.Location += new Point(0, -6);
+            }
+            if(keyState.IsKeyDown(down))
+            {
+                Bounds.Location += new Point(0, 6);
+            }
+        }
+
+        public void Draw(SpriteBatch sb, Texture2D texture2D)
+        {
+            sb.Draw(texture2D, Bounds, color);
+        }
+
+        public Vector2 Collison(Rectangle rectangle)
+        {
+            if(!rectangle.Intersects(Bounds))
+            {
+                return Vector2.One;
+            }
+
+            return new Vector2(-1, 1);
         }
     }
 }
